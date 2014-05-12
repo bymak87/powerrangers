faea6e9c31dc94802b9ebc5f89784713cb857924
Amy Bakameyer <bymak87@gmail.com>
2014-05-11 21:01:56 -0500

    person class

--- powerranger.rb
+++ powerranger.rb
@@ -1,4 +1,24 @@
 class Person
+  attr_accessor :caffeine_level
+  attr_reader :name
+
+  def initialize (name, caffeine_level)
+    @name = name
+    @caffeine_level = 0
+  end
+
+  def run
+    puts "#{name} is running."
+  end
+
+  def scream
+    puts "AHHHHHHHHHH!"
+  end
+
+  def drink_coffee
+    @caffeine_level +=1
+  end
+
 end
 
 class PowerRanger
